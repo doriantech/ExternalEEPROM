d92942f4c54d9d163375ee50321ab699234757ca
dorian-tech <50419628+dorian-tech@users.noreply.github.com>
2019-08-19 18:43:05 +0200

    Update ExternalEEPROM.h

--- ExternalEEPROM.h
+++ ExternalEEPROM.h
@@ -18,31 +18,31 @@ class EEProto {
       void operator <<=(uint8_t in);
       void operator >>=(uint8_t in);
     private:
-    long _index;
-    uint8_t _addr;
+      long _index;
+      uint8_t _addr;
 };
 
 
 class ExternalEEPROM {
     public:
-      ExternalEEPROM(long len, uint8_t addr);
+      ExternalEEPROM(long len, uint8_t addr = 0x80);
       EEProto operator[](long index);
-    uint8_t read(long index);
-    void write(long index, uint8_t data);
-    void update(long index, uint8_t data);
-    long length();
-  private:
-    long _length;
-    uint8_t _addr;
+      uint8_t read(long index);
+      void write(long index, uint8_t data);
+      void update(long index, uint8_t data);
+      long length();
+    private:
+      long _length;
+      uint8_t _addr;
 };
 
-#define 24C256 32768L
-#define 24C128 16384L
-#define 24C64 8192L
-#define 24C32 4096L
-#define 24C16 2048L
-#define 24C08 1024L
-#define 24C04 512L
-#define 24C02 256L
-#define 24C01 128L
+#define AT24C256 32768L
+#define AT24C128 16384L
+#define AT24C64 8192L
+#define AT24C32 4096L
+#define AT24C16 2048L
+#define AT24C08 1024L
+#define AT24C04 512L
+#define AT24C02 256L
+#define AT24C01 128L
 #endif
