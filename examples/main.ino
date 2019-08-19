297a7a2724af0c18dd3535a85244150f98042e57
dorian-tech <50419628+dorian-tech@users.noreply.github.com>
2019-08-19 14:10:39 +0200

    Update and rename main.cpp to examples/main.ino

---
+++ examples/main.ino
@@ -0,0 +1,9 @@
+#include <A.h>
+A x(2);
+void setup() 
+  Serial.begin(9600);
+  Serial.println(x[3]); //5
+}
+void loop() {
+
+}
--- main.cpp
+++
@@ -1,8 +0,0 @@
-//Example code
-#include <iostream>
-#include "A.h"
-int main() {
-  A x(2);
-  std::cout << x[3] //5
-  return 0;
-}
